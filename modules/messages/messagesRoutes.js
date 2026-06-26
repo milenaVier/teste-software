6cf52c57b964e9b9edbf90835f8ced68772e7acf
Milena <milenakostiwvier@gmail.com>
2026-06-26 15:31:23 -0300

    problema com o routes do item e do messages

--- modules/messages/messagesRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const messagesController = require('./messagesController');
-const isAuthenticated = require('../../middlewares/auth');
-
-router.post('/items/:id/messages', isAuthenticated, messagesController.create);
-
-module.exports = router;
