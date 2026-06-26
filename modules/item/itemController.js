a590aa06af4ae1edff80b2a5c908a48cf34a7a16
Milena <milenakostiwvier@gmail.com>
2026-06-26 15:20:01 -0300

    Alteração dos controllers

--- modules/item/itemController.js
+++ modules/item/itemController.js
@@ -5,5 +5,40 @@ exports.listarItens = async (req, res) => {
     const items = await itemService.getAllItem(type);
 
     res.render('views/item/index', { items });
+};
 
+exports.criarItem = async (req, res) => {
+    const { title, description, type, location} = req.body;
+    const userId = req.session.userId;
+
+    if (!userId) {
+        return res.redirect('/login');
+    }
+
+    const item = await itemService.createItem(
+        title,
+        description,
+        type,
+        location,
+        userId
+    );
+
+    res.redirect(`/item/${item.id}`);
+};
+
+exports.show = async (req, res) => {
+    const { id } = req.params;
+    const item = await itemService.getItemById(id);
+
+    res.render('views/item/show', { item });
+};
+
+exports.resolve = async (req, res) => {
+    const { id } = req.params;
+    const userId = req.session.userId;
+
+    await itemService.resolvedItem(id, userId);
+    req.flash('success', 'Item resolvido!');
+
+    res.redirect(`/item/${id}`);
 };
---
+++ modules/messages/messagesController.js
@@ -0,0 +1,20 @@
+const messagesService = require('./messagesService');
+
+exports.create = async (req, res) => {
+    const { content } = req.body;
+    const itemId = req.params.id;
+    const senderId = req.session.userId;
+
+    if (!senderId) {
+        return res.redirect('/login');
+    }
+
+    await messagesService.createMessage(
+        content,
+        itemId,
+        senderId
+    );
+    
+    req.flash('success', 'Mensagem enviada!');
+    res.redirect(`/item/${itemId}`);
+};
