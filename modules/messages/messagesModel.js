4ed50af36cd4cafafcb626033b48cc1d7aff09ee
Milena <milenakostiwvier@gmail.com>
2026-06-26 15:23:48 -0300

    Alteração no model

--- modules/messages/messagesModel.js
+++ modules/messages/messagesModel.js
@@ -5,7 +5,7 @@ const Messages = sequelize.define('Messages',
     {
         id:             { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
         content:        { type: DataTypes.STRING, allowNull: false },
-        itemPostId:     { type: DataTypes.INTEGER, allowNull: false },
+        itemId:         { type: DataTypes.INTEGER, allowNull: false },
         senderId:       { type: DataTypes.INTEGER, allowNull: false }
     },
     {
